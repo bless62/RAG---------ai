5fdefb3c59d1e37765e819f7451e1fba6b62d7c5
bless62 <38934653+bless62@users.noreply.github.com>
2026-05-17 16:52:06 +0800

    Update Google API Key to placeholder
    Replaced the Google API Key with a placeholder.

--- config_data.py
+++ config_data.py
@@ -1,7 +1,7 @@
 import os
 #向量知识库配置
 # 必须在这里配置你的 Google API Key，否则网页运行时会直接崩溃
-os.environ["GOOGLE_API_KEY"] = "AIzaSyCDdxngJ1vUHypfyOdC2qajIrUM-qHRh2g"
+os.environ["GOOGLE_API_KEY"] = "YOUR_GOOGLE_API_KEY"
 #向量知识库路径
 persist_directory = "D:/VscodeProjects/ai_project01/RAG与Agent开发入门/RAG项目案例-电商客服ai/chroma_db"
 #向量知识库模型名称
